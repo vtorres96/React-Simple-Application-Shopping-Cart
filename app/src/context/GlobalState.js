23e5d2f3b6b369d4fd584c8bc8b19b3b63e23a6d
vtorres96 <victorcdc96@gmail.com>
2020-03-23 16:49:04 -0300

    added GlobalState

---
+++ app/src/context/GlobalState.js
@@ -0,0 +1,41 @@
+import React, { useContext, useReducer } from 'react';
+
+import ShopContext from './shop-context';
+import { shopReducer, ADD_PRODUCT, REMOVE_PRODUCT } from './reducers';
+
+const GlobalState = props => {
+
+  const context = useContext(ShopContext)
+
+  // const [cart, setCart] = useState([]);
+  const [cartState, dispatch] = useReducer(shopReducer, { cart: [] });
+
+  const addProductToCart = product => {
+    setTimeout(() => {
+      // setCart(updatedCart);
+      dispatch({ type: ADD_PRODUCT, product: product });
+    }, 700);
+  };
+
+  const removeProductFromCart = productId => {
+    setTimeout(() => {
+      // setCart(updatedCart);
+      dispatch({ type: REMOVE_PRODUCT, productId: productId });
+    }, 700);
+  };
+
+  return (
+    <ShopContext.Provider
+      value={{
+        products: context.products,
+        cart: cartState.cart,
+        addProductToCart: addProductToCart,
+        removeProductFromCart: removeProductFromCart
+      }}
+    >
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
+
+export default GlobalState;
