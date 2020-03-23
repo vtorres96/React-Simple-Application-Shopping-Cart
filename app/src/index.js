a44e248cfcf16d099dd9bb38cd3e9910d93881d9
vtorres96 <victorcdc96@gmail.com>
2020-03-23 16:53:46 -0300

    added index of the application

---
+++ app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
+
+import App from './App';
+import shopReducer from './store/reducers';
+
+const store = createStore(shopReducer, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
